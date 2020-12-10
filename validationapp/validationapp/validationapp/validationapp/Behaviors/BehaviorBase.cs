bc7a79367669abc18ae0bd9ddf189250cd35d62f
Nii <ticialjc@gmail.com>
2020-12-10 14:22:13 +0800

    Sample for Validation
    Xamarin sample for similar feature to wpf ValidationRule

---
+++ validationapp/validationapp/validationapp/validationapp/Behaviors/BehaviorBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace validationapp.Behaviors
+{
+    public class BehaviorBase<T> : Behavior<T>
+    where T : BindableObject
+    {
+        #region Properties
+        public T AssociatedObject
+        {
+            get;
+            private set;
+        }
+        #endregion
+        #region NormalMethods
+        private void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+        #endregion
+        #region Overrides
+        protected override void OnAttachedTo(T bindable)
+        {
+            base.OnAttachedTo(bindable);
+            AssociatedObject = bindable;
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+        protected override void OnDetachingFrom(T bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+            AssociatedObject = null;
+        }
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+        #endregion
+    }
+}
