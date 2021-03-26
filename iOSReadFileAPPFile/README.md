2e51c29b97d214c6abdd15f54d2aece9b25c49cc
Nii <ticialjc@gmail.com>
2021-03-26 09:43:12 +0800

    Xamarin.iOS's application read the File app's file
    Xamarin.iOS's application read the File app's file

---
+++ iOSReadFileAPPFile/README.md
@@ -0,0 +1,5 @@
+# Solution
+
+Xamarin.iOS's application read the File app's file
+
+Demo project for https://docs.microsoft.com/en-us/answers/questions/329961/accessing-file-stored-in-34on-my-iphone34-in-ios-x.html?childToView=331115#answer-331115
---
+++ iOSReadFileAPPFile/file/file/ViewController.cs
@@ -0,0 +1,66 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace file
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            UIButton button = new UIButton() {
+
+                Frame = new CoreGraphics.CGRect(10, 100, 200, 80),
+                BackgroundColor = UIColor.LightGray,
+
+          };
+            button.SetTitle("Click", UIControlState.Normal);
+            button.TouchUpInside += Button_TouchUpInside;
+
+            View.AddSubview(button);
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        private void Button_TouchUpInside(object sender, EventArgs e)
+        {
+            string[] strs = {"public.image","public.text", };
+
+            //NSArray arr = NSArray.FromStrings(strs);
+
+            UIDocumentPickerViewController controller = new UIDocumentPickerViewController(strs,UIDocumentPickerMode.Open);
+
+
+            controller.Delegate = new PickerClass();
+
+            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(controller,true,null);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+
+
+    public class PickerClass : UIDocumentPickerDelegate, IUIDocumentInteractionControllerDelegate
+    {
+        public override void DidPickDocument(UIDocumentPickerViewController controller, NSUrl url)
+        {
+
+            var path = url.AbsoluteString;
+
+        }
+    }
+
+
+
+}
